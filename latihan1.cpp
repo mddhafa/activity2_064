f4ea71e8515fbd8cfb7960087dfe77e72681d323
mddhafa <mdmuhammaddhafa1234@gmail.com>
2023-03-08 11:51:33 +0700

    first commit

---
+++ latihan1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main (){ //begin
+
+    //Numeric Luas, P, L
+    double Luas, P, L;
+    //Display "Masukan Panjang = "
+    cout << "Masukan panjang = ";
+    //Accept P
+    cin >> P;
+    //Display "Masukan Lebarnya = "
+    cout << "Masukan Lebarnya = ";
+    //accept L
+    cin >> L;
+    //Compute Luas = P*L
+    Luas = P*L;
+    //Display "Luasnya = " = Luas
+    cout << "Luasnya = " << Luas;
+}//end
---
+++ latihan2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+double Luas, P, L;
+void input(){
+    cout << "Masukan Panjangnya = ";
+    cin >> P;
+    cout << "Masukan Lebarnya = ";
+    cin >> L;
+}
+
+double proses (){
+    Luas = P*L;
+}
+
+void output (){
+    cout << "Luasnya = " << Luas;
+}
+
+int main (){
+    input();
+    proses();
+    output();
+}
